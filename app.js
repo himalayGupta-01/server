91071fc81e406c5b811e11b1b356a83009b7cf65
himalayGupta-01 <himalay9644@gmail.com>
2025-08-27 18:31:24 +0530

    Changes done for URL

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv"
-import router from "./Routes/routes.js";
+import router from "./routes/routes.js";
 import { connectDB } from "./Database/db.js";
-// import path from "path";
 const app=express();
 const PORT= 8000;
 
@@ -13,16 +12,12 @@ dotenv.config();
 connectDB();
 app.use(cookieParser())
 app.use(cors({
-    origin:"*",
+    origin: process.env.PROD_URL,
     credentials:true,
 }));
 app.use(express.json())
 app.use("/",router);
 app.use(express.urlencoded({extended:false}));
-
-// const _dirname=path.dirname("");
-// const buildPath=path.join(_dirname,"../client/build")
-// app.use(express.static(buildPath));
 
 
 app.listen(PORT,()=>{
